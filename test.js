904f95da661d0b81cf39484e0a77d0e315b0373f
alkjoarizmy <Gymnastiar12345678@gmail.com>
2018-09-08 14:58:08 +0700

    Update test.js

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ const express = require('express');
 const axios = require('axios');
  
 const config = {
-  channelAccessToken: "",
-  channelSecret: "",
+  channelAccessToken: "bXsoHKBYFoZBEgp8ttWD7vFlDcepXmErffXdOYKk4HqyLek3eqMdFl5uoVDO4+u5iugvPvXsQiKYLrMReI7OXppR+Izzljz8A8qHVrTbEzpaQ93MCDRRLlsH57OdUjABn/jOC9n+yhQwCR7tJ/LGZQdB04t89/1O/w1cDnyilFU=",
+  channelSecret: "590e7297fcc2a1ac82fbaa81d7179af9",
 };
  
 // create LINE SDK client
